21e46c16525a8425657d8f2c326692c9c61b5989
luanmvcosta0 <luanmvcosta@mgail.com>
2024-10-06 10:46:58 -0300

    Exercicio 1/2

---
+++ src/exercicioIntroducao1_2/Funcionario.java
@@ -0,0 +1,17 @@
+package exercicioIntroducao1_2;
+
+public class Funcionario {
+
+    String nome;
+    String cargo;
+    double salario;
+
+    double calcularAumento(double porcentagem) {
+        return this.salario * (porcentagem / 100);
+    }
+    void aumentarSalario(double porcentagem) {
+        double aumento = this.calcularAumento(porcentagem);
+        this.salario += aumento;
+    }
+
+}
---
+++ src/exercicioIntroducao1_2/Main.java
@@ -0,0 +1,18 @@
+package exercicioIntroducao1_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome = "Luan";
+        funcionario.cargo = "Auxiliar de expedição";
+        funcionario.salario = 1992.90;
+
+        funcionario.aumentarSalario(20);
+
+        System.out.println(funcionario.salario);
+
+    }
+
+}
