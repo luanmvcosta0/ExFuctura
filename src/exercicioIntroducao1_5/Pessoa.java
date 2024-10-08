6d36382aeba5f4dd2b2b2b9b071267f9584d3c85
luanmvcosta0 <luanmvcosta@mgail.com>
2024-10-08 19:21:42 -0300

    Finalizando exercicio 1/5

---
+++ src/exercicioIntroducao1_5/Pessoa.java
@@ -0,0 +1,33 @@
+package exercicioIntroducao1_5;
+
+public class Pessoa {
+
+    String nome;
+    int idade;
+    String genero;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+}
