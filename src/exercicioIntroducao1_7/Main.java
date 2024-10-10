e882254ad75eb10266e0fea9f22a2191bce57f50
luanmvcosta0 <luanmvcosta@mgail.com>
2024-10-09 22:11:39 -0300

    Fazenmdo exercicio 1/7

---
+++ src/exercicioIntroducao1_7/Main.java
@@ -0,0 +1,19 @@
+package exercicioIntroducao1_7;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SerVivo serVivo = new SerVivo();
+
+        serVivo.setNome("Cavalo");
+        serVivo.setIdade(9);
+
+        System.out.println("Nome do Ser Vivo: " + serVivo.getNome());
+        System.out.println("Idade do Ser Vivo: " + serVivo.getIdade());
+
+
+
+    }
+
+}
---
+++ src/exercicioIntroducao1_7/SerVivo.java
@@ -0,0 +1,24 @@
+package exercicioIntroducao1_7;
+
+public class SerVivo {
+
+    public String nome;
+    public int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
